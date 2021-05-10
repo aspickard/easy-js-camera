1a00420b58a2e25ae22ddb73b6bbd316cb3aa23c
marc.nsd <mmg@notsodark.com>
2021-05-10 12:34:14 +0200

    Third commit

--- src/Camera.js
+++ src/Camera.js
@@ -1,142 +1,186 @@
-import CameraModel from "./CameraModel";
-export class Constraints {
-    constructor() {
+'use strict';
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.Constraints = undefined;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _CameraModel = require('./CameraModel');
+var _CameraModel2 = _interopRequireDefault(_CameraModel);
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var Constraints = exports.Constraints = function () {
+    function Constraints() {
+        _classCallCheck(this, Constraints);
         this.video = {
-            facingMode: 'environment'
+            facingMode: 'user'
         };
         this.audio = false;
     }
-    switchFacingMode(tryAgain = false) {
-        if(this.video.facingMode === 'user') {
-            this.video.facingMode = 'environment';
-        } else if(tryAgain) {
-            this.video.facingMode = {
-                exact: 'environment'
+    _createClass(Constraints, [{
+        key: 'switchFacingMode',
+        value: function switchFacingMode() {
+            var tryAgain = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
+            if (this.video.facingMode === 'user') {
+                this.video.facingMode = 'environment';
+            } else if (tryAgain) {
+                this.video.facingMode = {
+                    exact: 'environment'
+                };
+            } else {
+                this.video.facingMode = 'user';
             }
-        } else {
-            this.video.facingMode = 'user';
+            return this;
         }
-        return this;
-    }
-    getConstraint() {
-        return {
-            video: this.video,
-            audio: this.audio
+    }, {
+        key: 'getConstraint',
+        value: function getConstraint() {
+            return {
+                video: this.video,
+                audio: this.audio
+            };
         }
-    }
-}
-export default class Camera {
-    constructor(video, canvas) {
+    }]);
+    return Constraints;
+}();
+var Camera = function () {
+    function Camera(video, canvas) {
+        _classCallCheck(this, Camera);
         this.devices = [];
         this.stream = null;
         this.canvasElement = canvas;
         this.videoElement = video;
         this.constraints = new Constraints();
     }
-    getDevices() {
-        return new Promise(async (resolve, reject) => {
-            if(this.devices.length > 0) {
-                resolve(this.devices);
-                return;
-            }
-            try {
-                let devices = await navigator.mediaDevices.enumerateDevices();
-                devices.forEach(device => {
-                    if(device.kind && device.kind.toLocaleLowerCase() === 'videoinput')
-                        this.devices.push(new CameraModel(device));
-                });
-                resolve(this.devices);
-            } catch(error) {
-                console.error('GetDevices', error);
-                reject(error);
-            }
-        });
-    }
-    setVideoConstraints(videoConstraints) {
-        if(!this.constraints) {
-            this.constraints = new Constraints();
+    _createClass(Camera, [{
+        key: 'getDevices',
+        value: function getDevices() {
+            var _this = this;
+            return new Promise(async function (resolve, reject) {
+                if (_this.devices.length > 0) {
+                    resolve(_this.devices);
+                    return;
+                }
+                try {
+                    var devices = await navigator.mediaDevices.enumerateDevices();
+                    devices.forEach(function (device) {
+                        if (device.kind && device.kind.toLocaleLowerCase() === 'videoinput') _this.devices.push(new _CameraModel2.default(device));
+                    });
+                    resolve(_this.devices);
+                } catch (error) {
+                    console.error('GetDevices', error);
+                    reject(error);
+                }
+            });
         }
-        this.constraints.video = videoConstraints;
-        return this;
-    }
-    snap() {
-        this.canvasElement.width = this.videoElement.scrollWidth;
-        this.canvasElement.height = this.videoElement.scrollHeight;
-        let context = this.canvasElement.getContext('2d');
-        context.clearRect(0 , 0, this.canvasElement.width, this.canvasElement.height);
-        context.drawImage(this.videoElement, 0, 0, this.canvasElement.width, this.canvasElement.height);
-        return this.canvasElement;
-    }
-    /**
-     * @return String
-     */
-    snapAsBase64() {
-        this.snap();
-        let data = this.canvasElement.toDataURL('image/png');
-        return data;
-    }
-    /**
-     * @return Promise
-     */
-    snapAsBlob() {
-        this.snap();
-        return new Promise((resolve) => {
-            this.canvasElement.toBlob((blob) => {
-                resolve(blob);
-            }, 'image/png', 1);
-        });
-    }
-    start() {
-        return new Promise(async (resolve, reject) => {
-            try {
-                await this.getDevices();
-                let stream = await navigator.mediaDevices.getUserMedia(this.constraints.getConstraint());
-                this.videoElement.srcObject = stream;
-                this.stream = stream;
-                resolve();
+    }, {
+        key: 'setVideoConstraints',
+        value: function setVideoConstraints(videoConstraints) {
+            if (!this.constraints) {
+                this.constraints = new Constraints();
             }
-            catch(error) {
-                console.error('StartCamera', error);
-                reject(error);
-            }
-        });
-    }
-    stop() {
-        if(!this.videoElement && !this.videoElement.srcObject) return;
-        let tracks = this.videoElement.srcObject.getTracks();
-        tracks.forEach(track => track.stop());
-    }
-    switch(tryAgain = false) {
-        return new Promise(async (resolve, reject) => {
-            this.constraints = this.constraints.switchFacingMode(tryAgain);
-            this.stop();
-            try {
-                await this.start();
-                resolve();
-            }
-            catch(error) {
-                console.error('SwitchCamera', error);
-                reject(error);
-            }
-        });
-    }
-    
-    static isCameraSupported() {
-        return 'mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices;
-    }
-    static tryInvokePermission(video, canvas) {
-        return new Promise(async (resolve, reject) => {
-            try {
+            this.constraints.video = videoConstraints;
+            return this;
+        }
+    }, {
+        key: 'snap',
+        value: function snap() {
+            this.canvasElement.width = this.videoElement.scrollWidth;
+            this.canvasElement.height = this.videoElement.scrollHeight;
+            var context = this.canvasElement.getContext('2d');
+            context.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
+            context.drawImage(this.videoElement, 0, 0, this.canvasElement.width, this.canvasElement.height);
+            return this.canvasElement;
+        }
+        /**
+         * @return String
+         */
+    }, {
+        key: 'snapAsDataUrl',
+        value: function snapAsDataUrl() {
+            this.snap();
+            var data = this.canvasElement.toDataURL('image/png');
+            return data;
+        }
+        /**
+         * @return Promise
+         */
+    }, {
+        key: 'snapAsBlob',
+        value: function snapAsBlob() {
+            var _this2 = this;
+            this.snap();
+            return new Promise(function (resolve) {
+                _this2.canvasElement.toBlob(function (blob) {
+                    resolve(blob);
+                }, 'image/png', 1);
+            });
+        }
+    }, {
+        key: 'start',
+        value: function start() {
+            var _this3 = this;
+            return new Promise(async function (resolve, reject) {
+                try {
+                    await _this3.getDevices();
+                    var stream = await navigator.mediaDevices.getUserMedia(_this3.constraints.getConstraint());
+                    _this3.videoElement.srcObject = stream;
+                    _this3.stream = stream;
+                    resolve();
+                } catch (error) {
+                    console.error('StartCamera', error);
+                    reject(error);
+                }
+            });
+        }
+    }, {
+        key: 'stop',
+        value: function stop() {
+            if (!this.videoElement && !this.videoElement.srcObject) return;
+            var tracks = this.videoElement.srcObject.getTracks();
+            tracks.forEach(function (track) {
+                return track.stop();
+            });
+        }
+    }, {
+        key: 'switch',
+        value: function _switch() {
+            var _this4 = this;
+            var tryAgain = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
+            return new Promise(async function (resolve, reject) {
+                _this4.constraints = _this4.constraints.switchFacingMode(tryAgain);
+                _this4.stop();
+                try {
+                    await _this4.start();
+                    resolve();
+                } catch (error) {
+                    console.error('SwitchCamera', error);
+                    reject(error);
+                }
+            });
+        }
+    }], [{
+        key: 'isCameraSupported',
+        value: function isCameraSupported() {
+            return 'mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices;
+        }
+    }, {
+        key: 'tryInvokePermission',
+        value: function tryInvokePermission(video, canvas) {
+            return new Promise(async function (resolve, reject) {
+                try {
                     var devices = await navigator.mediaDevices.getUserMedia(new Constraints());
                     resolve(new Camera(video, canvas));
                     
                     devices.getTracks().forEach(function (track) {
-            return track.stop();
-         });
-            } catch(error) {
-                console.error('MediaDevices', error);
-                reject(error);
-            }
-        });
-    }
-}
+                return track.stop();
+             });
+                } catch (error) {
+                    console.error('MediaDevices', error);
+                    reject(error);
+                }
+            });
+        }
+    }]);
+    return Camera;
+}();
+exports.default = Camera;
